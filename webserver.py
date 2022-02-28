42e46227c68316d35f48394d4ea4ce860808d340
Perez <66048089+ThePerezenator@users.noreply.github.com>
2022-02-27 19:30:17 -0500

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -1,7 +1,5 @@
-from flask import Flask, flash, redirect, url_for, render_template, request, session, send_from_directory, abort, current_app as app
+from flask import Flask, redirect, url_for, render_template, request, abort, current_app as app
 from werkzeug.exceptions import HTTPException
-from urllib.parse import urlparse
-from werkzeug.utils import secure_filename
 import requests
 
 app = Flask(__name__, static_folder='static')
@@ -15,14 +13,22 @@ def block_method():
         print(f"BLOCKED: {bannedip}")
         abort(403)
 
-@app.errorhandler(HTTPException)
-def handle_exception(e):
+@app.errorhandler(404)
+def handle_exception(error):
     if request.remote_addr in bannedip:
-        print("yeap")
+        print("404 banned")
     else:
         bannedip.append(request.remote_addr)
-        print("maybe")
-    print(f"nope: {bannedip}")
+        print("banned")
+    return "forbidden", 403
+
+@app.errorhandler(505)
+def handle_exception(error):
+    if request.remote_addr in bannedip:
+        print("505 banned")
+    else:
+        bannedip.append(request.remote_addr)
+        print("banned")
     return "forbidden", 403
 
 @app.route('/favicon.ico')
@@ -31,15 +37,7 @@ def favicon():
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+    return redirect(url_for('ai'))
 
 @app.route("/sourcecode")
 def sc():
@@ -58,4 +56,4 @@ def ai():
         return render_template("ai.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
